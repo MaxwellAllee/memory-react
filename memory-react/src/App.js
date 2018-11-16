4c0be7703fa86c94a2c4380946e9fbed0a97a86d
Coding Monkey <Maxwell.W.Allee@gmail.com>
2018-11-16 00:38:41 -0500

    pushing to laptop

--- memory-react/src/App.js
+++ memory-react/src/App.js
@@ -4,36 +4,70 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
+let clickedArray = []
+let points = 0
+let highScore = 0
+let note
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    points,
+    highScore,
+    note:"click an image to begin"
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  pushArray = id => {
+    console.log(id)
+    if (!clickedArray.includes(id)){
+      clickedArray.push(id)
+      points++
+      note="You guessed correctly!"
+      if (points>highScore){
+        highScore=points
+      }
+      if (points === 12){
+        console.log("you win")
+        clickedArray.length=0
+        points=0
+      }
+    }
+    else{
+      console.log(`${id} in Array`)
+      note="Doh!!!!"
+      clickedArray.length=0
+      points=0
+    }
+    console.log(points)
+    this.setState({highScore, points, note})
   };
+  
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
+      
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+       
+        <Title>Clicky Game! 
+          <br/>Points: {this.state.points}
+          <br/>High Score: {this.state.highScore} 
+          <br/>{this.state.note} 
+           </Title>
+        
+        {this.state.friends.map((friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            pushArray={this.pushArray}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
-        ))}
+          
+        )))}
+  
       </Wrapper>
+     
     );
   }
 }
--- memory-react/src/components/FriendCard/FriendCard.js
+++ memory-react/src/components/FriendCard/FriendCard.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <span onClick={() => props.removeFriend(props.id)} className="remove">
+  <span onClick={() => props.pushArray(props.id)} className="pushIt">
     <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
     </div>
   </span>
+  
 
 );
 
